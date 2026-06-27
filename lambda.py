2aecad63f4d99717edf164522b63e762fcfdd406
Neoknight5 <anuradhajayale8@gmail.com>
2026-06-27 11:38:15 +0530

    Add lambda function to check if a number is even

---
+++ lambda.py
@@ -0,0 +1,8 @@
+sub = lambda a : print("this values is even")  if a%2 == 0  else  print("this value is nto even") 
+sub(10)
+# square = lambda a : a*a
+# val = int(input("enter your value :"))
+# print(square(val))
+# largest = lambda a,b : "5 is the biggest value" if a >b else "smaller" 
+# print(largest(5,6))
+
